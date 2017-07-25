6fb99c5333f601592131a36c73bc216b7737b4c5
Ashlyn N. Hussey <anhussey@DOM10523.local>
2017-07-25 13:43:42 -0500

    Chapter 7

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+	# Tests to be sure no changes to database if invalid user is created
+  	test "invalid signup information" do
+    	get signup_path
+    	assert_no_difference 'User.count' do
+      		post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    	end
+    	assert_template 'users/new'
+  	end
+
+  	# Test validating that a user was added to database after signing up
+  	test "Valid signup information" do 
+  		get signup_path
+  		# Assert that the user count is different by one after inserting user
+  		assert_difference 'User.count', 1 do
+  			post users_path, params: { user: {name: "Example User",
+  					email: "user@example.com",
+  					password: "password"
+  					password_confirmation: "password"}
+  			}
+  		end
+  		follow_redirect!	# Redirect page
+  		assert_template 'users/show'	# Confirm correct template
+  	end
+end
