34d97fb1ea38fdb18047afea5a9e93f5a692863d
Ashlyn N. Hussey <anhussey@DOM10523.local>
2017-07-25 16:31:23 -0500

    9.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
   	# If user exists and is authenticated, continue the log in process
   	if user && user.authenticate(params[:session][:password])
       log_in(user)  # Call log_in helper method
+      remember user   # Will assign user a permanent cookie until logout
       redirect_to user
   	else
       # Display error message
@@ -17,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,9 +6,25 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	# Remembers user for a period of time in a persistent session
+	def remember(user)
+		user.remember
+		cookies.permanent.signed[:user_id] = user.id 
+		cookies.permanent[:remember_token] = user.remember_token
+	end
+
 	# Return instance of the current logged in user, if one exists
+	# corresponding to the remember token cookie
 	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])	# Notice the OR-Equals operator
+		if (user_id = session[:user_id])
+			@current_user ||= User.find_by(id: user_id)
+		elsif (user_id = cookies.signed[:user_id])
+			user = User.find_by(id: user_id)
+			if user && user.authenticated?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
+		end
 	end
 
 	# Returns true if the user if logged in, false otherwise
@@ -16,8 +32,16 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	# Forgets the current user's persistent session
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+
 	# Logs out the current user
 	def log_out
+		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
 	end
