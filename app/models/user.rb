61edf1eab4deea227e3bed127840a06fc718c8bd
Ashlyn N. Hussey <anhussey@DOM10523.local>
2017-07-31 16:46:30 -0500

    11.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
-	# => before_save - Callback method invoked before instance is saved
-	before_save {self.email = email.downcase}	# Set user email to lowercase before save
 	# Validate presence of name attribute and that it is appropriate length
+    attr_accessor :remember_token, :activation_token
+    before_save   :downcase_email
+    before_create :create_activation_digest
 	validates(:name, presence: true, length: { maximum: 50})
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   	validates :email, presence: true, length: { maximum: 255 },
@@ -40,6 +40,17 @@ class User < ApplicationRecord
     # Forgets a users information upon logout
     def forget
     	update_attribute(:remember_digest, nil)
+    end
+
+     # Converts email to lower-case
+    def downcase_email
+        self.email = email.downcase 
+    end
+
+    # Creates and assigns the activation token and digest
+    def create_activation_digest
+        self.activation_token = User.new_token
+        self.activation_digest = User.digest(activation_token)
     end
 
 end
